953ae4ec75c55c0872f26349c6d1df61f22b6379
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2023-04-08 20:07:21 -0300

    feat: add abstract factory

--- src/main/java/design_patterns/creational/abstractfactory/Problem.java
+++ src/main/java/design_patterns/creational/abstractfactory/Problem.java
@@ -1,4 +1,12 @@
 package design_patterns.creational.abstractfactory;
 
+import design_patterns.creational.abstractfactory.operationalsystem.implementations.button.implementation.WindowsButton;
+
 public class Problem {
+
+  public static void main(String[] args) {
+    WindowsButton button = new WindowsButton();
+
+    button.props();
+  }
 }
